4338995ca916e19bd4529e6af9840a7855f0e9e1
dima dadyka <dima.dadyka06@gmail.com>
2025-09-28 11:10:58 +0300

    added task 2

--- lab3.py
+++ lab3.py
@@ -1,3 +1,8 @@
 #1
 squares = [x**2 for x in range(1, 11)]
 print("список квадротов чисел от 1 до 10\n",squares)
+
+
+#2
+even_numbers = [x for x in range(1, 20) if x % 2 == 0]
+print("список только четных чисел из диапазона 1-20\n",even_numbers)
